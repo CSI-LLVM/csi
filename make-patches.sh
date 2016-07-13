8a54cec4abab2c969c0fa5c2e379645f0c49fdc7
Tyler Denniston <tyler@csail.mit.edu>
2016-07-13 15:09:14 -0400

    Update make-patches.sh script.

--- make-patches.sh
+++ make-patches.sh
@@ -8,6 +8,9 @@ ignore_globs="*.gitignore *.gitmodules csi projects/compiler-rt tools/clang"
 # Name of the LLVM upstream remote.
 upstream_remote="llvm-origin"
 
+# Name of base branch.
+base_branch="${upstream_remote}/master"
+
 # Where to put the patches
 patchdir=$(readlink -en `pwd`)
 
@@ -44,13 +47,28 @@ function make_diff {
     patchfile="${patchdir}/${project}.patch"
     pushd $projectdir > /dev/null
     # Eval is necessary to prevent bash from quoting the exclude argument.
-    cmd="git diff -U999999 ${upstream_remote}/master...HEAD -- . ${exclude}"
+    cmd="git diff -U999999 ${base_branch}...HEAD -- . ${exclude}"
     eval $cmd > $patchfile
     echo "Wrote patch file $patchfile."
     popd > /dev/null
 }
 
+if [[ $1 == "--help" || $1 == "-h" ]]; then
+    echo "Creates diffs suitable for submission to reviews.llvm.org"
+    echo ""
+    echo "USAGE: $0 [branch]"
+    echo ""
+    echo "Optional argument [branch] specifies the name of a local branch to diff "
+    echo "against. If [branch] is not specified, defaults to ${base_branch}."
+    exit 1
+fi
+
+if [[ $1 != "" ]]; then
+    base_branch=$1
+fi
+
 check_remotes
+echo Creating diff against ${base_branch}
 make_diff ${llvm_root}
 make_diff ${clang_root}
 make_diff ${compilerrt_root}
